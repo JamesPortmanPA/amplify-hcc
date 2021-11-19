c610f24c0d1a72dfd4a5f7c1b9f6c298266f5b15
James Portman <James.Portman@paconsulting.com>
2021-11-19 11:02:05 +0000

    auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import Greeting from "./components/Greeting";
 
 function App() {
@@ -9,3 +10,31 @@ function App() {
 }
 
 export default App;
+*/ 
+//Old Code
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+import Amplify from "aws-amplify";
+//import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+        <Authenticator usernameAttributes='email'/>
+      </header>
+    </div>
+    
+  );
+}
+
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//New stuff for AWS Cognito
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+//----------------------------------
 
 ReactDOM.render(
   <React.StrictMode>
